e1535ee79b6c0058dfcc66fb7ae2f70fcfd4f04b
Brodie Owens <brodie@onyxclient.com>
2025-07-28 13:13:17 +1000

    rewrite config to make minimal using nightly

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-### Neovim Config
+# Neovim Config
 
 ```
 git clone https://github.com/OnyxWM/Neovim-config
 ```
+Minimal Neovim Config using Native Package Manager. 
+Must use Nightly 0.12 version of Neovim to use the Native Package Manager.
+
+## Plugins
+- Treesitter
+- Nvim-lsp
+- Blink-cmp
+- Telescope
+- Lualine
+- Tokyo Night Theme
 
 
--- init.lua
+++ init.lua
@@ -1,31 +1,83 @@
-print("sourced")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.signcolumn = "yes"
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
 
-require("config.lazy")
+vim.keymap.set('n', "<leader>o", ':update<CR> :source<CR>')
+vim.keymap.set('n', "<leader>w", ':write<CR>')
+vim.keymap.set('n', "<leader>q", ':quit<CR>')
 
-vim.opt.shiftwidth = 4
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.pack.add({
+    { src = "https://github.com/folke/tokyonight.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = 'make' },
+    { src = "https://github.com/nvim-lualine/lualine.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim" },
+    { src = "https://github.com/MunifTanjim/nui.nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/saghen/blink.cmp", version = 'v1.3.1' },
 
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
 })
---[[vim.api.nvim_create_autocmd('TermOpen', {
-  nvim_create_augroupoup = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
-}) --]]
+require('blink.cmp').setup({
+    keymap = { preset = 'default' },
+    appearance = {
+        nerd_font_variant = 'normal'
+    },
+    completion = { documentation = { auto_show = false } },
+    sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
 
-vim.keymap.set('n', '<space>st', function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd('J')
-  vim.api.nvim_win_set_height(0, 15)
-end)
+    fuzzy = {
+        implementation = "lua",
+    },
+})
+
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+vim.lsp.enable({ "lua_ls", "html", "phpactor" })
+vim.lsp.config('lua_ls', {
+    capabilities = capabilities,
+})
+vim.lsp.config('html', {
+    capabilities = capabilities,
+})
+vim.lsp.config('phpactor', {
+    capabilities = capabilities,
+})
+
+vim.keymap.set('n', "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+vim.cmd("colorscheme tokyonight")
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'tokyonight'
+    }
+}
+
+require('nvim-treesitter.configs').setup {
+    ensured_installed = { "lua", "vim" },
+    auto_install = false,
+    highlight = {
+        enable = true
+    }
+}
+
+require("neo-tree").setup({})
+
+vim.keymap.set('n', "<leader>fd", require('telescope.builtin').find_files)
+vim.keymap.set('n', "<leader>fh", require('telescope.builtin').help_tags)
+vim.keymap.set('n', "<leader>ff", ':Neotree<CR>')
